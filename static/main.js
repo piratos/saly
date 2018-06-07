756fbf2e2cf575d938451b1474f78552e9f0a34a
sayf eddine hammemi <sayf.piratos@gmail.com>
2018-06-07 23:12:11 +0200

    Add tasks on the fly (No Persistence/backend yet).

--- static/main.js
+++ static/main.js
@@ -1,7 +1,18 @@
 // TEST
+function addItem(){
+  var before_last = $("#sortable li:last")[0];
+  var taskname = $('#in_taskname')[0]
+  var li = document.createElement("li");
+  var taskname_div = document.createElement('div');
+  taskname_div.innerHTML = taskname.value;
+  li.appendChild(taskname_div);
+  before_last.before(li);
+  $('#in_taskname').val('');
+}
+
 $(document).ready(function(){
 var changed = function( event, ui ) {
-  };
+};
 var theone = $("#sortable").sortable({change: changed});
 $("#sortable").disableSelection();
 
