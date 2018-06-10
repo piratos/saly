403b29234f4b91d43ff17f199529343589e64dfb
sayf eddine hammemi <sayf.piratos@gmail.com>
2018-06-10 16:10:30 +0200

    add auth urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^addItem/(?P<taskname>.+)/$', views.add_task),
     url(r'^delItem/(?P<id>\d+)/$', views.remove_task),
     url(r'^status/(?P<id>\d+)/(?P<status>.+)/$', views.set_status),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
